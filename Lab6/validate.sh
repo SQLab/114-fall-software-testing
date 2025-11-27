03afc2d17d03e85b906d35511c91e27aeec94986
as535364 <me@ujoj.cc>
2025-11-27 18:59:36 +0800

    fix: update scoring script

--- Lab6/validate.sh
+++ Lab6/validate.sh
@@ -13,6 +13,20 @@ cp $solution_path/ans .
 cp $solution_path/chal .
 cp $solution_path/*.txt .
 
+if [ "$1" -eq 1 ]; then
+  if grep -q "[^a-zA-Z0-9]" 1.txt; then
+    echo "[!] 1.txt contains non-alphanumeric characters."
+    exit 1
+  fi
+fi
+
+if [ "$1" -eq 2 ]; then
+  if ! grep -q "[^[:print:]]" 2.txt; then
+    echo "[!] 2.txt must contain at least one non-printable character."
+    exit 1
+  fi
+fi
+
 make run$1 > out
 result=$(diff --strip-trailing-cr ans out)
 if [[ -n $result ]]; then
